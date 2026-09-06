03b5818ba25bdc78a20e5df8b1fbd8af9fb7bce9
gainen <morishirogainen@gmail.com>
2026-09-06 15:13:35 +0200

    enemytype

--- Assets/Scripts/Melee Siren.cs
+++ Assets/Scripts/Melee Siren.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class MeleeSiren : MonoBehaviour
+public class MeleeSiren : Basesiren
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
